99675eb1ff65fdad7ffb33eaec4b1b5d66d29ce2
Neharsha <nehabavala9@gmail.com>
2023-06-09 12:57:46 +0000

    correct-solution

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -10,5 +10,4 @@ const NotFound = () => (
     <h1 className="not-found-heading">Not Found</h1>
   </div>
 )
-
 export default NotFound
--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,16 +1,15 @@
 // Write your JS code here
 import './index.css'
 
-const UserInfo = () => {
-  ;<div className="user-info-container">
+const UserInfo = () => (
+  <div className="user-info-container">
     <img
       src="https://assets.ccbp.in/frontend/react-js/profile-img.png "
       className="profile-image"
       alt="profile"
     />
-    <h1 className="head">Waddw Warren</h1>
+    <h1 className="head">Wade Warren</h1>
     <p className="p">Software Developer at UK</p>
   </div>
-}
-
+)
 export default UserInfo
